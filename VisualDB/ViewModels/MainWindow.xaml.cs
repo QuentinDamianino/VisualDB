a859b8ea739e09ed1f6043536f63376404cca5f4
QuentinDamianino <brdej98@gmail.com>
2020-01-22 23:52:47 +0100

    add exit functionality

--- VisualDB/ViewModels/MainWindow.xaml.cs
+++ VisualDB/ViewModels/MainWindow.xaml.cs
@@ -24,5 +24,29 @@ namespace VisualDB
         {
             InitializeComponent();
         }
+
+        private void ExitCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand
+        (
+            "Exit",
+            "Exit",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.F4, ModifierKeys.Alt)
+            }
+        );
     }
 }
